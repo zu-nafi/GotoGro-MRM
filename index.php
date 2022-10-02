f5bbc853bb02280631e257994b71ef5c28d392b4
zu-nafi <113743703+zu-nafi@users.noreply.github.com>
2022-10-03 09:30:38 +1100

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8"/>
-        <meta name="description" content="Managing Software Projects"/>
-        <meta name="keywords" content="HTML, CSS, Grocery, MRM, Project"/>
+
         <link href= "style.css" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
 
-        <title>Goto Gro MRM</title>
+        <title>GotoGro-MRM</title>
     </head>
 
     <body>  
         <div id="header">
             <div id="titleboxone"></div>
                 <div id="titleboxtwo">
-                    <h1 id="titleheading">Goto Grocery</h1>
+                    <h1 id="titleheading">GotoGro-MRM</h1>
                 </div>
             </div>
 
@@ -32,19 +31,20 @@
         <div>
             <h2>Background</h2>
             <p id="homebody">
-                Goto (GotoGro) Inc. is a Hawthorn-based member-owned grocery store. They provide high-quality grocery items to customers in need through delivery.<br/><br/>
-                The company used a paper-based register system to write and record the members' information and sales records. However, they have found it difficult to manage the members' expectations and their requirements using that system. They particularly struggled to meet their members' grocery needs as they are not able to gather their member’s requirements in a time-efficient manner. <br/><br/>
-                The company required a software application that will assist them in managing their members' records, storing those records, and informing them of their grocery needs in an instant. The implementation of this web-based software system can help them solve all these problems.
+                Goto Grocery is a grocery shop based in Hawthorn that provides high-quality groceries to its customers. They use a paper-based method to record customer information and sales data. Recently, they found it challenging to manage the expectations of their customers and meet their food demands. They often ordered products that customers did not need.
+				They need software to assist them in managing customer data and promptly informing them of customers' shopping requirements.
+
             </p>
 
             <h3>Project Requirements</h3>
             <p id="homebodyp">
-                The GotoGro-MRM project is a graphical user application that will manage member grocery order details and sales records. To be able to fulfill the requirements of GotoGro, the software will need to track customer purchases and product stock. <br/><br/>
-                This will require a database system which will read and store data from user input from a graphical user interface for data input. The software will have to contain multiple databases, one for storing purchases and sales, and another that will record member details. The graphical interface will allow the user to add, search, edit, and delete both member records and sale records.<br/><br/>
-                The software will also be able to generate reports of the data that can be converted into a CSV format to easily identify how much stock of each item will be needed based on demand.
+                The Members Record Management (MRM) software will replace the current paper-based system for record-keeping. The software will feature GUI (Graphical User Interface) that will be user-friendly and will display member, sales and item records.
+				The software will solve many persistent problems associated with the previous data storage system (Book-based system). The programme must feature cloud storage and a user-friendly Graphical User Interface (GUI). It should also display record history and be categorised in a variety of ways.
+
             </p>
             <img src="grocerystore.jpeg" id="groceryimage" />
         </div>
+
 
     </body> 
 </html>
